6f15073d03685a2976a77f1821114b5a82c7203e
Adityavanakudre <57445614+Adityavanakudre@users.noreply.github.com>
2024-11-02 23:07:24 -0700

    Create Config.java

---
+++ src/main/java/com/inventory/Controller/Config.java
@@ -0,0 +1,22 @@
+package com.inventory.Controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server productionServer = new Server();
+        productionServer.setUrl("https://satisfied-magic-production.up.railway.app");
+        productionServer.setDescription("Production Server");
+
+        return new OpenAPI().addServersItem(productionServer);
+        
+//        issue with the service
+    }
+}
