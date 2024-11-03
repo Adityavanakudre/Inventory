5139daf1cf79cbfa92bc8f326af15c6936172709
Adityavanakudre <57445614+Adityavanakudre@users.noreply.github.com>
2024-11-02 19:14:32 -0700

    Update InventoryApplication.java

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -4,14 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-	@OpenAPIDefinition(
-    info = @Info(title = "Your API", version = "1.0.0", description = "API Documentation"),
-    servers = {
-        @Server(url = "https://satisfied-magic-production.up.railway.app", description = "Production Server")
-    }
-)
-	
-	
 public class InventoryApplication {
 
 	public static void main(String[] args) {
